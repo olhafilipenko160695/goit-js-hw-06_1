e44cf2bbf147431edd54f6d3764e75d598e9fcda
olhafilipenko160695 <olyaao95@gmail.com>
2023-09-30 20:52:59 +0200

    1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const container = document.querySelector('#ingredients');
+
+for (var i = 0; i < ingredients.length; i++) {
+  const item = document.createElement('li'); // Створюємо  <li> 
+  item.textContent = ingredients[i]; // Встановлюємо текст для <li> 
+  item.classList.add('item');// Встановлюємо клас для <li>
+  container.appendChild(item);
+}
