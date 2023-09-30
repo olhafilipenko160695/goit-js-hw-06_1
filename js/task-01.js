c2c468028e70baefe8fa2dcf712425a1c8c295f4
olhafilipenko160695 <olyaao95@gmail.com>
2023-09-30 20:23:01 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
 
 //* Кількість категорій в ul#categories
+
 const items = document.querySelectorAll('.item');
 console.log("Number of categories:" + items.length);
 
-const newArray = [...items];
+//* текст заголовку елемента і кількість елементів в категорії
 
+const newArray = [...items];
 newArray.forEach(element => {
     console.log("Category:" + element.firstElementChild.textContent);
     console.log("Elements:" + element.lastElementChild.children.length);
